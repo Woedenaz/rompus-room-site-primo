a667d4a5263bee5fa6ec99fd344a26aa8bdce412
Woed E. Naz <leminnes@gmail.com>
2023-12-14 12:37:08 -0500

    Updating to work with bun and fly.io

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@catdadcode/svelte-adapter-bun'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess({
     postcss: true
-	}),
-	kit: {
-		adapter: adapter()
-	},
-	vitePlugin: {
-		inspector: false
-	},
-	onwarn: (warning, handler) => {
-		if(warning.filename.includes('svelte-json-tree')) {return}
-		handler(warning);
-	}
-};
+  }),
+  kit: {
+    adapter: adapter()
+  },
+  vitePlugin: {
+    inspector: false
+  },
+  onwarn: (warning, handler) => {
+    if (warning.filename.includes('svelte-json-tree')) {
+      return
+    }
+    handler(warning)
+  }
+}
 
-export default config;
+export default config
